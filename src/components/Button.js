2221a2be78569f93c49ff672f39fe79dd9b44e92
jonathan-best <jonathan.best@ft.com>
2018-05-03 14:17:40 +0100

    Fix quote issue

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,21 @@
 export default {
   root: {
-    border: `1px solid #0D7680`,
-    borderRadius: `0px`,
+    border: '1px solid #0D7680',
+    borderRadius: '0px',
     color: '#0D7680',
     backgroundColor: '#FFFFFF',
     padding: '5px 5px',
     textTransform: 'none',
     '&$disabled': {
-      border: `1px solid rgba(13,118,128,.4)`,
-      color: `rgba(13,118,128,.4)`
+      border: '1px solid rgba(13,118,128,.4)',
+      color: 'rgba(13,118,128,.4)'
     },
     '&:hover': {
       backgroundColor: 'rgba(13,118,128,.1)'
     },
     '&:active': {
-      background: `rgba(13,118,128,1)`,
-      color: `white`
+      background: 'rgba(13,118,128,1)',
+      color: 'white'
     }
   },
   flatPrimary: {
@@ -36,19 +36,19 @@ export default {
     }
   },
   flatSecondary: {
-    border: `1px solid rgb(38,42,51)`,
+    border: '1px solid rgb(38,42,51)',
     color: 'rgb(38,42,51)',
     backgroundColor: '#FFFFFF',
     '&$disabled': {
-      border: `1px solid rgba(38,42,51,.4)`,
-      color: `rgba(38,42,51,.4)`
+      border: '1px solid rgba(38,42,51,.4)',
+      color: 'rgba(38,42,51,.4)'
     },
     '&:hover': {
       backgroundColor: 'rgba(38,42,51,.1)'
     },
     '&:active': {
-      background: `rgba(38,42,51,1)`,
-      color: `white`
+      background: 'rgba(38,42,51,1)',
+      color: 'white'
     }
   }
 }
