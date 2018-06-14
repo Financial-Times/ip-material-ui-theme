6f51ff831603ddfe94b0a0b7e3a792128f469860
jonathan-best <jonathan.best@ft.com>
2018-06-14 08:46:39 +0100

    Update input for new MUI release

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,32 +1,35 @@
 export default {
   formControl: {
-     'label + &': {
-       marginTop: '0px'
-     }
-    },
-    multiline : {
-      padding: '25px 6px 7px 6px'
-    },
-    root: {
-      marginTop: '0',
-      padding: '25px 6px 7px 6px',
-      '&$focused': {
-        padding: '24px 5px 6px 5px',
-        border: '1px solid #00EAFF'
-      }
-    },
-    underline: {
-      '&:after': {
-        height: 0
-      },
-      '&$focused:after': {
-        transform: 'scaleX(0)'
-      },
-      '&:hover:not($disabled):before': {
-        height: 0
-      },
-      '&:before': {
-        height: 0
-      }
+    'label + &': {
+      marginTop: '0px'
     }
+  },
+  multiline : {
+    padding: '25px 6px 7px 6px'
+  },
+  root: {
+    marginTop: '0',
+    padding: '25px 6px 7px 6px',
+    '&$focused': {
+      padding: '24px 5px 6px 5px',
+      border: '1px solid #00EAFF'
+    }
+  },
+  underline: {
+    '&:before': {
+      borderBottom: 'none'
+    },
+    '&:after': {
+      borderBottom: 'none'
+    },
+    '&$focused:after': {
+      transform: 'scaleX(0)'
+    },
+    '&:hover:not($disabled):not($focused):not($error):before': {
+      borderBottom: 'none'
+    },
+    '&:before': {
+      borderBottom: 'none'
+    }
+  }
 }
